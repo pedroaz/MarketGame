575fcb557e4f660996c21d89811807f6c0550ce4
pedroaz <pedro.azvm@gmail.com>
2021-03-25 18:17:33 -0300

    Returning

--- MarketGame/ClientApp/src/app/models/stockCertificate.ts
+++ MarketGame/ClientApp/src/app/models/stockCertificate.ts
@@ -2,6 +2,6 @@ import { Stock } from "./stock";
 
 export interface StockCertificate{
     stock: Stock;
-    Amount: number;
-    ValueWhenBought: number;
+    amount: number;
+    valueWhenBought: number;
 }
--- MarketGame/ClientApp/src/app/tables/stock-certificate-table/stock-certificate-table.component.ts
+++ MarketGame/ClientApp/src/app/tables/stock-certificate-table/stock-certificate-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Stock } from 'src/app/models/stock';
+import { StockCertificate } from 'src/app/models/stockCertificate';
 
 @Component({
   selector: 'app-stock-certificate-table',
@@ -7,7 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockCertificateTableComponent implements OnInit {
 
-  constructor() { }
+  stockCertificates : StockCertificate[] = [];
+
+  constructor() { 
+    this.stockCertificates.push(<StockCertificate>{
+      amount: 10,
+      stock: <Stock>{
+        name: "stock_1"
+      }
+    });
+
+    this.stockCertificates.push(<StockCertificate>{
+      amount: 5,
+      stock: <Stock>{
+        name: "stock_2"
+      }
+    });
+
+    this.stockCertificates.push(<StockCertificate>{
+      amount: 200,
+      stock: <Stock>{
+        name: "stock_3"
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
